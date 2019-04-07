21729d746b819178c22ae5b8fce5e17ec56b556d
rondo <patrick04184@gmail.com>
2019-04-07 22:36:45 +0800

    4/7 upload

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="laptop">
+    <div class="navigation">
+      <div class="navigation__logo">
+        <img src="@/assets/image/logo-all-dark.png" alt="logo" class="navigation__logo-img">
+      </div>
+
+      <ul class="navigation__item">
+        <li class="navigation__item-menu">
+          <router-link to="/">Home</router-link>
+        </li>
+        <li class="navigation__item-menu">
+          <a href="#">Dessert</a>
+        </li>
+        <li class="navigation__item-menu">
+          <a href="#">Login</a>
+        </li>
+        <li class="navigation__item-menu">
+          <a href="#" class="fas fa-shopping-cart"></a>
+        </li>
+      </ul>
     </div>
     <router-view/>
   </div>
@@ -10,5 +26,4 @@
 
 <style lang="scss">
 @import "./assets/main";
-
 </style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,40 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-
-    <button @click="logfn">123</button>
+    <!-- 首頁的圖片大圖 -->
+    <img src="@/assets/image/slide.jpg" alt="slide" class="home__img">
+    <div class="topic">
+      <div class="topic__item">
+        <img src="@/assets/image/topic_1.jpg" alt="topic1" class="topic__item-img">
+        <p class="topic__item-font">本日精選</p>
+      </div>
+      <div class="topic__item">
+        <img src="@/assets/image/topic_2.jpg" alt="topic2" class="topic__item-img">
+        <p class="topic__item-font">人氣推薦</p>
+      </div>
+      <div class="topic__item">
+        <img src="@/assets/image/topic_3.jpg" alt="topic3" class="topic__item-img">
+        <p class="topic__item-font">新品上市</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { login } from '@/api/user';
+import { login } from "@/api/user";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld';
+import HelloWorld from "@/components/HelloWorld";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld,
+    HelloWorld
   },
-  methods:{
-      logfn(){
-          
-        //   const data ={
-        //       email:'qd@gmailc.com',
-        //       password:'0000'
-        //   }
-        //   login(data.email, data.password).then(response => {
-        //   const data = response.data;
-        //   if (data.roles && data.roles.length > 0) { // 驗證返回的roles是否是一个非空陣列
-        //     commit('SET_ROLES', data.roles)
-        //   } else {
-        //     reject('getInfo: roles must be a non-null array !')
-        //   }
-        // }).catch(error => {
-        //     console.log(error);
-        // });
-      }
+  methods: {
+    logfn() {
+      //   const data ={
+      //       email:'qd@gmailc.com',
+      //       password:'0000'
+      //   }
+      //   login(data.email, data.password).then(response => {
+      //   const data = response.data;
+      //   if (data.roles && data.roles.length > 0) { // 驗證返回的roles是否是一个非空陣列
+      //     commit('SET_ROLES', data.roles)
+      //   } else {
+      //     reject('getInfo: roles must be a non-null array !')
+      //   }
+      // }).catch(error => {
+      //     console.log(error);
+      // });
+    }
   }
 };
 </script>
+<style>
+</style>
+
