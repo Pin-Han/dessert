c4c75458c2efb234a8bd2f5c3d5c353ed84470a8
Rondo_lin <patrick04184@gmail.com>
2019-05-07 11:54:02 +0800

    upload

--- src/components/ToDashboard/index.vue
+++ src/components/ToDashboard/index.vue
@@ -3,20 +3,18 @@
     <div class="section-admin">
       <div class="admin">
         <div class="admin__title">後台登入</div>
-        <p class="admin__text">Email</p>
         <div class="admin__input">
-          <i class="fas fa-user-cog"></i>
-          <input type="text" class="admin__content" placeholder="Type your Email">
+          <i class="fas fa-user-cog admin__input-icon"></i>
+          <input type="email" class="admin__input-content" placeholder="Type your Email">
         </div>
 
-        <p class="admin__text">Password</p>
         <div class="admin__input">
-          <i class="fas fa-lock"></i>
-          <input type="text" class="admin__content" placeholder="Type your password">
+          <i class="fas fa-lock admin__input-icon"></i>
+          <input type="password" class="admin__input-content" placeholder="Type your password">
         </div>
 
-        <a href="#">Login</a>
-        <a href="#">返回</a>
+        <a href="#" class="admin__login ">Login</a>
+         <router-link to="/" class="admin__back">&larr; Back</router-link>
       </div>
     </div>
   </div>
