c41d9131acb9ddfede31fe3444f75a822c7274a2
rondo <patrick04184@gmail.com>
2019-05-07 09:53:38 +0800

    upload

--- src/components/ToDashboard/index.vue
+++ src/components/ToDashboard/index.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>
-        <div class="admin">
-            
+  <div>
+    <div class="section-admin">
+      <div class="admin">
+        <div class="admin__title">後台登入</div>
+        <p class="admin__text">Email</p>
+        <div class="admin__input">
+          <i class="fas fa-user-cog"></i>
+          <input type="text" class="admin__content" placeholder="Type your Email">
         </div>
+
+        <p class="admin__text">Password</p>
+        <div class="admin__input">
+          <i class="fas fa-lock"></i>
+          <input type="text" class="admin__content" placeholder="Type your password">
+        </div>
+
+        <a href="#">Login</a>
+        <a href="#">返回</a>
+      </div>
     </div>
+  </div>
 </template>
