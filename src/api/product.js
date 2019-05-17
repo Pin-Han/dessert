b6b5cb7278aef6860b4fdab79a178a074074a659
Rondo_lin <patrick04184@gmail.com>
2019-05-17 17:29:35 +0800

    upload

--- src/api/product.js
+++ src/api/product.js
@@ -19,7 +19,7 @@ export function getproduct(id) {
 //前台 加入購物車
 export function addtocart(data) {
   return request({
-    url: `/api/${process.env.VUE_APP_CUSTOMPATH}/product/cart`,
+    url: `/api/${process.env.VUE_APP_CUSTOMPATH}/cart`,
     method: 'post',
     data:{
       //傳送的資料
@@ -27,4 +27,17 @@ export function addtocart(data) {
   });
 }
 
-
+//前台 取得購物車列表
+export function getcartinfo() {
+  return request({
+    url: `/api/${process.env.VUE_APP_CUSTOMPATH}/cart`,
+    method: 'get',
+  });
+}
+//前台 刪除某一筆購物車
+export function deletecart(id) {
+  return request({
+    url: `/api/${process.env.VUE_APP_CUSTOMPATH}/cart/${id}`,
+    method: 'delete',
+  });
+}
